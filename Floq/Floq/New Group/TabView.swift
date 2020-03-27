e2f9760c4c56c944cb9800a44d91addd20cc01c5
Codesworth <codezworth@gmail.com>
2020-03-27 23:50:51 +0000

    Minor Adds

---
+++ Floq/Floq/New Group/TabView.swift
@@ -0,0 +1,17 @@
+//
+//  TabView.swift
+//  Floq
+//
+//  Created by Shadrach Mensah on 27/03/2020.
+//  Copyright © 2020 Arun Nagarajan. All rights reserved.
+//
+
+import UIKit
+
+
+//class TabView:UIView{
+//    
+//    lazy var commentTab:UIButton = {
+//        let but = UIButton(frame:.
+//    }()
+//}
